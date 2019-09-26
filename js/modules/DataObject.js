3ebba776af529dcdedfa131eb089c4e9c8f2e6d7
Luisa Valero <luisa.valero19@gmail.com>
2019-09-25 23:05:55 -0400

    Practiced in class

---
+++ js/modules/DataObject.js
@@ -0,0 +1,32 @@
+// export default
+const Team = {
+  Trevor:{
+    name: "Trevor",
+    role: "Professor",
+    bio: "the sad, sad story of my life",
+    avatar: "olaf.jpeg"
+  },
+
+Justin:{
+    name: "Justin",
+    role: "Professor/Coordinator",
+    bio: "we are not sure, but probably something evil",
+    avatar: "justin.jpeg"
+  },
+
+
+Joe:{
+  name: "Joe",
+  role: "Professor",
+  bio: "I love hunny",
+  avatar: "pooh.jpeg"
+},
+
+Jarrod:{
+  name: "Jarrod",
+  role: "Professor",
+  bio: "I have more than one look",
+  avatar: "zoolander.jpg"
+},
+  }
+  export default Team;
